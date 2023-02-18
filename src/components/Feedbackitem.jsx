f1377ac370ed7921c9462c458c3a27b6c310be7f
Ozan-sert <ozansert1993@gmail.com>
2023-02-18 02:18:42 -0500

    final code

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useContext } from 'react';
+import { FaTimes, FaEdit } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
 
-function Feedbackitem() {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState("this is an example of Feedback item")
+import FeedbackContext from '../context/FeedbackContext';
 
-    
-  return (
-    <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div> 
-    </div>
-  );
-}
+const FeedbackItem = ({ item }) => {
+    const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+    const { id, rating, text } = item;
+    return (
+        <Card>
+            <div className="num-display">{rating}</div>
+            <button onClick={() => deleteFeedback(id)} className="close">
+                <FaTimes color="purple" />
+            </button>
+            <button onClick={() => editFeedback(item)} className="edit">
+                <FaEdit color="purple" />
+            </button>
+            <div className="text-display">{text}</div>
+        </Card>
+    );
+};
 
-export default Feedbackitem;
+FeedbackItem.propTypes = {
+    item: PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+    }),
+};
+
+export default FeedbackItem;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,36 @@
-import PropTypes from 'prop-types'
-function Header({ text }) {
-  return (
-    <header>
-        <div className="container">
-            <h2>{text}</h2>
-        </div>
-    </header>
-  )
-}
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-Header.defaultProps = {
-    text :  'Feedback UI'
-}
+const Header = ({ text, bgColor, textColor }) => {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    };
+    const headerTextStyles = {
+        color: textColor,
+        textDecoration: 'none',
+    };
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <Link to="/" style={headerTextStyles}>
+                    <h2>{text}</h2>
+                </Link>
+            </div>
+        </header>
+    );
+};
 
 Header.propTypes = {
-    text :  PropTypes.string,
-}
-export default Header
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+};
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
+};
+
+export default Header;
